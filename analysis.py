8913336e77349afebfef5ba79270bed9e1c7a7e6
Mike Fortin <msfortin@buffalo.edu>
2019-10-06 15:51:50 -0400

    Taller fighter win percentage

--- analysis.py
+++ analysis.py
@@ -24,8 +24,25 @@ for fight in red_or_blue:
         red_wins += 1
 #print(red_wins)
 red_win_pct = decimal_to_percentage(float(red_wins) / total_fights)
+
+red_height = data.R_Height_cms
+blue_height = data.B_Height_cms
+
+taller_fighter_wins = 0
+
+for i in range(0,total_fights):
+    if red_height[i] > blue_height[i] and red_or_blue[i] == "Red":
+        taller_fighter_wins += 1
+    elif blue_height[i] > red_height[i] and red_or_blue[i] == "Blue":
+        taller_fighter_wins += 1
+
+taller_win_pct = decimal_to_percentage(float(taller_fighter_wins) / total_fights)
+
+#Outut Below
 print("\nUFC Data Analysis by Mike Fortin")
+print("\nStatistics based on " + str(total_fights) + " fights between 1993 and 2019...")
 print("\nThe red corner wins " + str(red_win_pct) + "% of the time.")
+print("\nThe taller fighter wins " + str(taller_win_pct) + "% of the time. ")
 
 
 
